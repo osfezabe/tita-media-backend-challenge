8dd8cefefd2f181eda7e213548ef6074ef8a667a
Oscar Zapata <oscarfernandozapata@hotmail.com>
2024-01-14 13:20:49 -0500

    Add liquibase configuration

--- src/main/java/com/titamedia/challenge/TechnicalTestApplication.java
+++ src/main/java/com/titamedia/challenge/TechnicalTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TechnicalTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechnicalTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTestApplication.class, args);
+    }
 
 }
--- src/main/java/com/titamedia/challenge/domain/User.java
+++ src/main/java/com/titamedia/challenge/domain/User.java
@@ -14,7 +14,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String dni;
+    private String document;
     private String firstName;
     private String lastName;
     private String email;
